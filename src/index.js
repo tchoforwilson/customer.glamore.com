8e019e31aab38b3a536ad3030c747a1b2e36b228
tchoforwilson <tchoforwilson@gmail.com>
2023-12-20 22:17:49 +0100

    Imported css tailwind style

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter as Router } from "react-router-dom";
+import "./assets/styles/css/main.css";
 import "./assets/styles/sass/main.scss";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
