36f5140fc1d27ce2b310656a55d4ad5115eeab98
tchoforwilson <tchoforwilson@gmail.com>
2023-12-23 19:27:16 +0100

    Filter form values

---
+++ src/screens/home/FilterValues.js
@@ -0,0 +1,69 @@
+/**
+ * FILTER VALUES FOR SHOP SCREEN
+ */
+// 1. By date
+export const dates = [
+  { name: "RECENT", value: "recent" },
+  { name: "THIS MONTH", value: "month" },
+  { name: "THIS YEAR", value: "year" },
+  { name: "ANYTIME", value: "anytime" },
+];
+// 2. By others
+export const sortBy = [
+  { name: "RATING", value: "rating" },
+  { name: "POPULARITY", value: "popularity" },
+  { name: "FEATURED", value: "featured" },
+  { name: "LOCATION", value: "location" },
+];
+// 3. By brands
+export const brands = [
+  { name: "ADDIDAS", value: "addidas" },
+  { name: "VOGUE", value: "vogue" },
+  { name: "LUIS VUITTON", value: "luis vuitton" },
+  { name: "NIKE", value: "nike" },
+];
+// 4. By date
+export const genders = [
+  { name: "MEN", value: "men" },
+  { name: "WOMEN", value: "women" },
+  { name: "BOYS", value: "boys" },
+  { name: "GIRLS", value: "girls" },
+];
+// 5. By sizes
+export const sizes = [
+  { name: "SMALL SIZE", value: "small" },
+  { name: "MEDIUM", value: "medium" },
+  { name: "MEDIUM PLUS", value: "plus" },
+  { name: "LARGE", value: "large" },
+  { name: "EXTRA LARGE", value: "xl" },
+  { name: "XXL", value: "xxl" },
+];
+// 6. By deals
+export const deals = [
+  { name: "COUPONS", value: "coupons" },
+  { name: "DISCOUNT", value: "discount" },
+  { name: "BULK BUYS", value: "bulk-buys" },
+  { name: "2 FOR 1", value: "2-for-1" },
+];
+// 7. By materials
+export const materials = [
+  { name: "COTTON", value: "cotton" },
+  { name: "POLYESTER", value: "polyester" },
+  { name: "SILK FABRIC", value: "silk" },
+  { name: "TRADITIONAL", value: "traditional" },
+  { name: "LEATHER", value: "leather" },
+  { name: "FUR", value: "fur" },
+  { name: "WOOL", value: "wool" },
+];
+// 8. By colors
+export const colors = [
+  { name: "pink", value: "#9757D7" },
+  { name: "dark-gray", value: "#353945" },
+  { name: "red", value: "#EF466F" },
+  { name: "grey", value: "#707784" },
+  { name: "gray", value: "#545454" },
+  { name: "orange", value: "#FB941B" },
+  { name: "colonial", value: "#002E63" },
+  { name: "green", value: "#45B26B" },
+  { name: "blue", value: "#0F0AE4" },
+];
