3b4c4d45b33bb51e28e2323c88e37549d650cfdd
tchoforwilson <tchoforwilson@gmail.com>
2023-12-27 04:36:23 +0100

    Reordered

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter as Router } from "react-router-dom";
-import "./assets/styles/css/main.css";
-import "./assets/styles/sass/main.scss";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+import "./assets/styles/css/main.css";
+import "./assets/styles/sass/main.scss";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
