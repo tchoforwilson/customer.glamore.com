915dd604d5bfd52c64640051bb4b4e3084fe5fcd
tchoforwilson <tchoforwilson@gmail.com>
2023-12-27 20:14:22 +0100

    Submit button

---
+++ src/components/buttons/SubmitButton.js
@@ -0,0 +1,27 @@
+import React from "react";
+import Spinner from "../common/Spinner";
+import { useFormikContext } from "formik";
+
+/**
+ * @brief A simple submit button
+ * @param {String} name -> Name of button
+ * @param {Boolean} loading -> Loading spinner
+ * @param {Any} otherProps -> Other properties
+ * @return {JSX}
+ */
+const SubmitButton = ({ title, loading, ...otherProps }) => {
+  const { handleSubmit } = useFormikContext();
+  return (
+    <button
+      disabled={loading}
+      type="submit"
+      {...otherProps}
+      onClick={handleSubmit}
+    >
+      {/**TODO: Remember to add spinner */}
+      <span className="btn-submit__title">{title}</span>
+    </button>
+  );
+};
+
+export default SubmitButton;
