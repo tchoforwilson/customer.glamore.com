f8ab7d9d60231d57bb586e5b6132390350da901a
Minister <mundestephane13@gmail.com>
2023-12-30 04:46:48 +0100

    Migrated Product Card from the previous code base

--- src/components/cards/ProductCard.js
+++ src/components/cards/ProductCard.js
@@ -1,82 +1,54 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 import { currencyFormatter } from "../../utilities/functions";
+import styles from "./ProductCard.module.scss";
 
 const ProductCard = ({ product, ...otherProps }) => {
+  const navigate = useNavigate();
+  const handleClick = (id) => () =>
+    navigate(navigate(`/product/${id}`, { replace: true }));
   return (
-    <figure className="card product-card" {...otherProps}>
-      <img
-        className="product-card__img"
-        alt={product.name}
-        src={product.imageUrl}
-      />
-      <figcaption className="product-card__details">
-        <div className="flex justify-between">
-          <h3 className="product-card__name">{product.name}</h3>
-          <div className="product-card__price">
-            <span>{currencyFormatter(product.currency, product.price)}</span>
-          </div>
-        </div>
-        <div className="product-card__rating">
-          <span className="product-card__rating-text">Avg Rating:</span>
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke-width="1.5"
-            stroke="currentColor"
-            data-slot="icon"
-            class="w-6 h-6 icon product-card__rating-icon"
-          >
-            <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              d="M11.48 3.499a.562.562 0 0 1 1.04 0l2.125 5.111a.563.563 0 0 0 .475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 0 0-.182.557l1.285 5.385a.562.562 0 0 1-.84.61l-4.725-2.885a.562.562 0 0 0-.586 0L6.982 20.54a.562.562 0 0 1-.84-.61l1.285-5.386a.562.562 0 0 0-.182-.557l-4.204-3.602a.562.562 0 0 1 .321-.988l5.518-.442a.563.563 0 0 0 .475-.345L11.48 3.5Z"
-            />
-          </svg>
+    <div className="" {...otherProps}>
+      {/* The product image goes below */}
+      <div className="relative w-full rounded-md shadow-sm shadow-gray-400/40 bg-orange-300 aspect-[13/16]">
+        <img
+          src={product.img}
+          alt={product.name}
+          className="size-full object-cover object-center"
+        />
 
-          <span className="product-card__rating-quantity">
-            {product.ratingsQuantity}
-          </span>
-        </div>
-        <div className="flex justify-between mt-8">
-          <div className="product-card__cart">
-            <span>add to cart</span>
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke-width="1.5"
-              stroke="currentColor"
-              data-slot="icon"
-              class="w-6 h-6 icon"
-            >
-              <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 0 0-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 0 0-16.536-1.84M7.5 14.25 5.106 5.272M6 20.25a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Zm12.75 0a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Z"
-              />
-            </svg>
-          </div>
-          <button className="product-card__favorite-btn">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke-width="1.5"
-              stroke="currentColor"
-              data-slot="icon"
-              class="w-6 h-6 icon"
-            >
-              <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12Z"
-              />
-            </svg>
+        <span className="absolute rounded-full top-[5%] left-[5%] px-4 py-2 bg-white text-black">
+          New
+        </span>
+      </div>
+
+      {/* the contents of the product goes below */}
+      <div className="flex flex-col gap-1 mt-2">
+        <div className="flex justify-between gap-4 items-start">
+          <h3 className="text-xl font-bold truncate">{product.name}</h3>
+          <button className="bg-black text-white text-center font-black py-2 px-4 rounded-md">
+            {product.currency} {product.price}
           </button>
         </div>
-      </figcaption>
-    </figure>
+
+        <div className="flex gap-2">
+          <span className="text-sm">Avg Rating: </span>
+          <span className="text-sm">4.2</span>
+        </div>
+
+        <div className="flex gap-2 mt-4 justify-between items-center">
+          <button className="rounded-full border border-gray-300 px-4 py-2">
+            Add to cart
+          </button>
+          <span
+            role="button"
+            className="text-sm rounded-full border border-gray-300 p-4 shadow shadow-gray-400"
+          >
+            O
+          </span>
+        </div>
+      </div>
+    </div>
   );
 };
 
