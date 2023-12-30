6c6e56b7e88f914f398c711e8ed9af081cd7d2c6
Minister <mundestephane13@gmail.com>
2023-12-30 04:24:13 +0100

    Created the category card component

---
+++ src/components/cards/CategoryCard.js
@@ -0,0 +1,24 @@
+import React from "react";
+import styles from "./CategoryCard.module.scss";
+
+export default function CategoryCard({ category, className = '', ...otherProps }) {
+  return (
+    <div className={`${styles["category-card"]} ${className}`} style={{
+      '--background': category.background ?? 'firebrick',
+      '--foreground': category.foreground ?? 'white',
+    }}>
+      <div className={styles["category-card__image"]}>
+        <img
+          src={category.image}
+          alt={`Glamore ${category.name}`}
+        />
+      </div>
+      <div className={styles["category-card__content"]}>
+        <span className={styles["category-title"]}>{category.name}</span>
+        <span className={styles["category-card__info"]}>
+          {`${category.products?.length ?? 0} products`}
+        </span>
+      </div>
+    </div>
+  );
+}
