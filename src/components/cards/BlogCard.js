87ee175c7551f6541f072a4f7c1929185d8e059b
Minister <mundestephane13@gmail.com>
2023-12-30 04:24:02 +0100

    Created the blog card component

---
+++ src/components/cards/BlogCard.js
@@ -0,0 +1,17 @@
+import styles from "./ArticleCard.module.scss";
+
+export default function ArticleCard({ article, className = "" }) {
+  return (
+    <article className={`${styles["blog-card"]} ${className}`}>
+      <div className={styles["blog-card__inner"]}>
+        <img
+          src={article.image}
+          alt={article.title}
+          className={styles["blog-card__image"]}
+        />
+      </div>
+      <div className={styles["blog-card__overlay"]}></div>
+      <h3 className={styles["blog-card__title"]}>{article.title}</h3>
+    </article>
+  );
+}
